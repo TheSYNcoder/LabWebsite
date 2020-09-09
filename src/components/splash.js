b67662dfd416d9ae8169ec508e02d396bd716eb4
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-09-10 01:33:19 +0530

    particles

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,18 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Particles from 'react-particles-js';
+import splashCSS from  "./splash.css";
+const options = {
+  "particles": {
+      "number": {
+          "value": 160,
+          "density": {
+              "enable": false
+          }
+      },
+      "size": {
+          "value": 7,
+          "random": true
+      },
+      "move": {
+          "direction": "top",
+          "out_mode": "out"
+      },
+      "line_linked": {
+          "enable": false
+      }
+  },
+  "interactivity": {
+      "events": {
+          "onclick": {
+              "enable": true,
+              "mode": "remove"
+          },
+          "onhover": {
+            "enable": true,
+            "mode": "repulse"
+        }
+      },
+      "modes": {
+          "remove": {
+              "particles_nb": 10
+          }
+      }
+  }
+};
 
-export default () => (
-  <section className="hero has-background-danger is-fullheight">
-    <div className="hero-body">
-      <div className="container">
-        <h1 className="title">
-            Welcome to DVLP
-        </h1>
-        <h2 className="subtitle">
-            A lab based in Jadavpur University
-        </h2>
-        <Link to="/home" className="button is-rounded has-background-primary">Welcome</Link>
+const Splash = () => {
+  return(
+    // <div>
+    <section className="hero has-background-danger is-fullheight">
+  <Particles  className= {`${splashCSS.particles}`} params={options} />
+      <div className="hero-body ">
+        <div className="container">
+          <div  style={{zIndex:2}} >
+          <h1 className="title" >
+              Welcome to DVLP
+          </h1>
+          <h2 className="subtitle pd-4">
+              A lab based in Jadavpur University
+          </h2>
+           </div>
+          <br />
+          <Link to="/home" className="button is-rounded has-background-primary pt-4" style={{zIndex:6}}>Welcome</Link>
+
+        </div>
       </div>
-    </div>
-  </section>
-);
+  {/* </Particles> */}
+    </section>
+    // </div>
+  )
+};
+
+
+export default Splash; 
+
