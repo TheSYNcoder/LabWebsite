f67957e2bc5dc1ff4be99b8115770c6913f971cb
theSYNcoder <papaigd@gmail.com>
2020-09-09 22:16:00 +0530

    add

--- src/components/splash.js
+++ src/components/splash.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => (
-  <section className="hero is-info is-fullheight">
+  <section className="hero has-background-danger is-fullheight">
     <div className="hero-body">
       <div className="container">
         <h1 className="title">
-            Welcome to ReactPWA
+            Welcome to DVLP
         </h1>
         <h2 className="subtitle">
-            An extendable boilerplate built on top of PawJS, for developers
+            A lab based in Jadavpur University
         </h2>
-        <Link to="/home" className="button">Checkout the full demo</Link>
+        <Link to="/home" className="button is-rounded has-background-primary">Welcome</Link>
       </div>
     </div>
   </section>
