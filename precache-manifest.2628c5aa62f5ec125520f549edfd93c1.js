9312fe5323ffa08d01e358c5ac1b3a17c89adb50
Shuvayan Ghosh Dastidar <papaigd@gmail.com>
2022-01-10 17:42:17 +0530

    Updates

---
+++ precache-manifest.2628c5aa62f5ec125520f549edfd93c1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f5ce25b39f39554245528b00f88540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c14e3b16cf7520cd6d",
+    "url": "/static/css/2.5481e53c.chunk.css"
+  },
+  {
+    "revision": "8849dd7e8dd05fe0c6f5",
+    "url": "/static/css/main.434c54ad.chunk.css"
+  },
+  {
+    "revision": "21c14e3b16cf7520cd6d",
+    "url": "/static/js/2.1bc43d94.chunk.js"
+  },
+  {
+    "revision": "1faa91311bcc96e9878bd4f5d3cebdec",
+    "url": "/static/js/2.1bc43d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8849dd7e8dd05fe0c6f5",
+    "url": "/static/js/main.145ca7e7.chunk.js"
+  },
+  {
+    "revision": "b825611e65a010d30be1",
+    "url": "/static/js/runtime-main.f95d7b59.js"
+  },
+  {
+    "revision": "0f7b35f520e3533890fc84e3f8669efe",
+    "url": "/static/media/bg.0f7b35f5.jpg"
+  },
+  {
+    "revision": "2d457942dca8dc966956d71d8562cb27",
+    "url": "/static/media/bis.2d457942.jpeg"
+  },
+  {
+    "revision": "937e35fc59532a25a154b17ad53de62b",
+    "url": "/static/media/img.937e35fc.jpeg"
+  },
+  {
+    "revision": "4a062bfd52483fc720aadb807f446ca3",
+    "url": "/static/media/ju.4a062bfd.jpeg"
+  },
+  {
+    "revision": "0a9da55c9e4efc10088dd93254dd0df1",
+    "url": "/static/media/lab1.0a9da55c.jpg"
+  },
+  {
+    "revision": "384251b5725ec21c90d71d7eab451f88",
+    "url": "/static/media/lab2.384251b5.jpg"
+  },
+  {
+    "revision": "6f0c7313e1ff882878bea28e4e8347c6",
+    "url": "/static/media/lab3.6f0c7313.jpg"
+  },
+  {
+    "revision": "cbf98c471649970bfa1b1d435e0d0479",
+    "url": "/static/media/logo.cbf98c47.jpg"
+  },
+  {
+    "revision": "f6a5af61ffcf211291f75b9519c0863a",
+    "url": "/static/media/logoName.f6a5af61.png"
+  },
+  {
+    "revision": "e36e15755abb31088f6dd59d8b6b5d73",
+    "url": "/static/media/shuv.e36e1575.jpg"
+  }
+]);
